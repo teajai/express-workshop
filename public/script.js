05ec03c5e21407dbbb8d66e44123144952e48f7e
teajai <tania@MacBookPro-2016.local>
2017-03-02 23:17:42 +1100

    Completed step 9 including re-write of script.js to handle more types of browsers
    Completed step 9 including re-write of script.js to handle more types
    of browsers
    
    Fetch - which is used throughout the original script.js is considered
    experimental and not supported by all desktops or any mobile browsers
    at all.
    
    Changed this code and tested on my little local wifi network to work
    with various desktops (including emulations) android mobile and iPhone
    iOS (both latest) - may still not work on earlier versions

--- public/script.js
+++ public/script.js
@@ -2,86 +2,90 @@
 // https://developer.mozilla.org/en/docs/Web/API/Fetch_API
 // So if using a browser not supporting fetch then get an alert to try
 // another browser for this test
-if (typeof fetch !== "function") {
-  alert("This exercise is using an experimental function 'fetch' which is not supported by your browser. For this learning lesson can we suggest the latest version of Firefox or Chrome. ");
+
+// if (typeof fetch !== "function") {
+//   alert("Cannot handle fetch ");
+// }
+
+// Rewritten to use $.ajax rather than fetch
+// Tested using various desktops and local network with mobile phones
+
+// Called when document is ready
+$(document).ready(function() {
+
+  // Setup form so when submitted don't go down normal path but use this
+  // process instead
+  $("form").submit(function(event){
+    //event.preventDefault(); Stops page being submitting - handled by false
+    // return - see below
+    postBlogItem(this); // this is the DOM object
+
+    return false; // Return false stops page being submitted as per normal action
+                  // submitting a form - don't need preventDefault as commented
+                  // out above
+                  // More explanation of the differences
+                  // http://stackoverflow.com/questions/1357118/event-preventdefault-vs-return-false
+  });
+
+  // Now get the posts from the server to add to the bottom of the page
+  // This gets all the posts returned
+  $.ajax({
+      url: '/get-posts',
+      success: function(data) {
+        addBlogpostsToPage (data);
+      },
+      error: function(error){
+          console.log(error);
+      }
+  });
+});
+
+
+// The user has submitted an item now send the item to the server and get
+// back an OK sign to then just add that item to the list at the bottom
+// of the page
+function postBlogItem (form) { // Passed DOM object of form
+  var jform = $(form); // Now create JQuery object from DOM object
+  // console.log(form); // Dom object
+  // console.log("-----------------------------");
+  // console.log(jform); // Jquery object
+  // console.log(jform.serializeArray()); // Format {name:"blogpost", value="blah de blah"}
+  // console.log("-----------------------------");
+  // console.log(jform.serialize()); // Format 'blogpost=blah de blah'
+
+  $.ajax({
+      url: jform.attr("action"), // using JQuery obj or 'form.action' (DOM object)
+      type: "POST",
+      data: jform.serialize(), // Format 'blogpost=blah de blah'
+      // OR instead of the above 'data' line can have the following 2 lines
+      // data: jform.serializeArray(), // Format {name:"blogpost", value="blah de blah"}
+      // dataType: 'json', // Need to indicate the format
+      success: function(data) {
+        addBlogpostsToPage (data);
+        form.reset(); // Or 'jform[0].reset()' if wanting to use JQuery object
+      },
+      error: function(error){
+          console.log(error);
+      }
+  });
+
 }
-else {
-  if (document.readyState !== 'loading') {
-    ready();
-  } else {
-      document.addEventListener('DOMContentLoaded', ready);
-  }
-}
 
-function ready () {
-    getBlogposts('/get-posts');
-
-    // send posts to server
-    var form = document.querySelector('form');
-    form.addEventListener('submit', function (event) {
-
-        event.preventDefault(); // prevents the form from contacting our server automatically (we want to do it ourselves)
-        var formActionUrl = form.action; // 'form.action' is the url '/create-post'
-        var formData = new FormData(form);
-
-        postBlogposts(formActionUrl, formData);
-    });
-}
-
-/****
- * Function definitions
- ***/
-function postBlogposts (url, data) {
-    fetch(url, {
-        method: 'POST',
-        body: data
-    })
-    .then(function (res) {
-        res.json()
-            .then(function (json) {
-                console.log(json);
-                addBlogpostsToPage(json);
-                document.querySelector('form').reset();
-        })
-    })
-    .catch(function (err) {
-        console.error(err)
-    });
-}
-
-function getBlogposts (url) {
-    fetch(url, {
-        method: 'GET'
-    })
-    .then(function (res) {
-        res.json()
-        .then(function (json) {
-            console.log(json);
-            addBlogpostsToPage(json);
-        });
-    })
-    .catch(function (err) {
-        console.error(err)
-    });
-}
-
+// Add items in 'data' to the list of items at the bottom of the page
 function addBlogpostsToPage (data) {
-    for (var blogpost in data) {
-        if (data.hasOwnProperty(blogpost)) {
+  for (var blogPost in data) {
+      var postDiv         = document.createElement('div');
+      var postText        = document.createElement('p');
+      var thumbnail       = document.createElement('img');
+      var postContainer   = document.getElementsByClassName('post-container')[0];
 
-            var postDiv         = document.createElement('div');
-            var postText        = document.createElement('p');
-            var thumbnail       = document.createElement('img');
-            var postContainer   = document.querySelector('.post-container');
+      thumbnail.src = "./img/logo2.png";
+      thumbnail.className = "thumbnail";
+      postText.innerHTML = data[blogPost];
+      postDiv.className = "post";
 
-            thumbnail.src = "./img/logo2.png";
-            thumbnail.className = "thumbnail";
-            postText.innerHTML = data[blogpost];
-            postDiv.className = "post";
-
-            postDiv.appendChild(thumbnail);
-            postDiv.appendChild(postText);
-            postContainer.appendChild(postDiv);
-        }
-    }
+      postDiv.appendChild(thumbnail);
+      postDiv.appendChild(postText);
+      postContainer.appendChild(postDiv);
+  }
 }
